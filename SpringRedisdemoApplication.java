2dfe158bf7c290f10fc8f940d3a24166b73bdb65
vijayakumar-sellappan <134674337+vijayakumar-sellappan@users.noreply.github.com>
2023-07-03 20:30:29 +0530

    Add files via upload

---
+++ SpringRedisdemoApplication.java
@@ -0,0 +1,18 @@
+package com.example.SpringRedisdemo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+
+@SpringBootApplication
+public class SpringRedisdemoApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SpringRedisdemoApplication.class, args);
+	}
+
+}
